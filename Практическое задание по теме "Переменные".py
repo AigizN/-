e16ee076a9e0cd63acb744fd77be780cb38873ed
AigizN <aigiznag@gmail.com>
2024-10-21 21:19:43 +0500

    Add files via upload

---
+++ Практическое задание по теме "Переменные".py
@@ -0,0 +1,5 @@
+number_of_completed_works = 12 #Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+number_of_hours_spent = 1.5
+course_name = "Pyton"
+time_for_one_job = (number_of_hours_spent / number_of_completed_works)
+print('Курс:', course_name,'всего задач:', number_of_completed_works, 'затрачено часов:', number_of_hours_spent, 'среднее время выполнения:', time_for_one_job)
